82a5da011456bcb46133ef77f460556631da9777
BarkerProoks <jonparkerbrooks@gmail.com>
2024-12-02 23:19:10 +0000

    lua sucks

--- src/day2.lua
+++ src/day2.lua
@@ -1,4 +1,22 @@
+local function checkLevels(levels)
+    local n = #levels
+    local up = levels[n] - levels[n - 1] < 0
+
+    while n > 1 do
+        local v = levels[n] - levels[n - 1]
+
+        if (v == 0) or ((v > 0 and up) or (v < 0 and not up)) or (math.abs(v) > 3) then
+            return false
+        end
+
+        n = n - 1
+    end
+
+    return true
+end
+
 local part1 = 0
+local part2 = 0
 
 -- supposedly the file closes automatically at the end of this loop
 for line in io.lines('input/day2.txt') do
@@ -9,39 +27,23 @@ for line in io.lines('input/day2.txt') do
         levels[#levels+1] = tonumber(token)
     end
 
-    local n = #levels -- count down from this number
-    local goingUp = levels[n] - levels[n - 1] < 0 -- get the first direction
-    -- its okay if they are equal, the while loop below will detect that and
-    -- mark it as invalid regardless
-    local isSafe = true -- assume it's a safe record
-
-    while n > 1 do -- going until n == 2
-
-        -- sliding window of [n, n - 1]
-        local v = levels[n] - levels[n - 1]
-
-        -- the 3 if statements below act as a filter based on the problem statement
-        if v == 0 then
-            isSafe = false
-            break
-        end
-
-        if (v > 0 and goingUp) or (v < 0 and not goingUp) then
-            isSafe = false
-            break
-        end
-        
-        if math.abs(v) > 3 then
-            isSafe = false
-            break
-        end
-
-        n = n - 1 -- decrement by 1
+    if checkLevels(levels) then -- only add to the total if it's safe
+        part1 = part1 + 1
     end
 
-    if isSafe then -- only add to the total if it's safe
-        part1 = part1 + 1
+    -- O(n * (m - 1)) ... yikes
+    -- luckily m is usually small
+    for i in pairs(levels) do
+        -- test a new levels array with the current index removed
+        local levels_copy = { table.unpack(levels) }
+        table.remove(levels_copy, i)
+
+        if checkLevels(levels_copy) then
+            part2 = part2 + 1
+            break -- as soon as we find a valid levels table, break
+        end
     end
 end
 
-print(part1)
+print(string.format("part 1: %d", part1))
+print(string.format("part 2: %d", part2))
