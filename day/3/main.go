11c05665a1c3c437f2cb3db5f772873491e3a128
BarkerProoks <jonparkerbrooks@gmail.com>
2024-12-03 17:23:49 +0000

    day 3 part 2 done... damn that was easy?

--- day/3/main.go
+++ day/3/main.go
@@ -7,6 +7,11 @@ import (
 	"strconv"
 )
 
+type doLocation struct {
+	state bool
+	index int
+}
+
 func main() {
 
 	data, err := os.ReadFile("./day/3/input.txt")
@@ -15,12 +20,25 @@ func main() {
 		panic(err)
 	}
 
+	src := string(data)
+
 	mulRegex := regexp.MustCompile("mul\\([0-9]+,[0-9]+\\)")
 	numRegex := regexp.MustCompile("[0-9]+")
+	doRegex := regexp.MustCompile("(do\\(\\)|don't\\(\\))")
+	
+	doQueue := make([]doLocation, 0)
+	do := true
 
 	part1 := 0
+	part2 := 0
 
-	for _, token := range mulRegex.FindAllString(string(data), -1) {
+	for _, indicies := range doRegex.FindAllStringIndex(src, -1) {
+		token := src[indicies[0]:indicies[1]]
+		doQueue = append(doQueue, doLocation{token == "do()", indicies[0]})
+	}
+
+	for _, indicies := range mulRegex.FindAllStringIndex(src, -1) {
+		token := src[indicies[0]:indicies[1]]
 		numbers := numRegex.FindAllString(token, 2)
 		
 		x, err := strconv.Atoi(numbers[0])
@@ -34,7 +52,22 @@ func main() {
 		}
 
 		part1 += x * y
+	
+		// if our current index is greater than the front of the queue's
+		// then we have passed a do or a don't. change the state before
+		// calculating
+		if len(doQueue) > 0 {
+			if indicies[0] > doQueue[0].index {
+				do = doQueue[0].state // change state
+				doQueue = doQueue[1:] // pop the front of the queue
+			}	
+		}
+		
+		if do {
+			part2 += x * y
+		}
 	}
 
-	fmt.Println(part1)
+	fmt.Printf("part 1: %d\n", part1)
+	fmt.Printf("part 2: %d\n", part2)
 }
