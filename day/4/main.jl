91565dff2376bedc4f41629f9fe787c12b14b19d
BarkerProoks <jonparkerbrooks@gmail.com>
2024-12-09 22:04:24 +0000

    solved day 4 part 2. gotta keep up

--- day/4/main.jl
+++ day/4/main.jl
@@ -1,3 +1,9 @@
+struct Letter
+    score::Int8
+    x::Int64
+    y::Int64
+end
+
 function search(line) # was waaaay overthinking. sliding window of 4
     count = 0
     for i in length(line):-1:4
@@ -9,6 +15,20 @@ function search(line) # was waaaay overthinking. sliding window of 4
         end
     end
     return count
+end
+
+function searchmas(letters) # sliding window of 3
+    locations = []
+    for i in length(letters):-1:3
+        queue = letters[i - 2 : i]
+        if length(queue) == 3
+            scores = [letter.score for letter in queue]
+            if scores == [2, 3, 4] || scores == [4, 3, 2]
+                push!(locations, queue[2]) # get middle letter location (A)
+            end
+        end
+    end
+    return locations
 end
 
 # the idea here is to convert chars to ints and
@@ -26,6 +46,7 @@ height, width = length(scores), length(scores[1])
 wordsearch = reshape(collect(Iterators.flatten(scores)), width, height)
 
 part1 = 0
+part2 = 0
 
 for y in 1:height
     global part1
@@ -38,23 +59,33 @@ for x in 1:width
 end
 
 rows = (width * height) - 1
-nesw, nwse = [Vector{Int8}(undef, 0) for _ in 1:rows], [Vector{Int8}(undef, 0) for _ in 1:rows]
+nesw, nwse = [Vector{Letter}(undef, 0) for _ in 1:rows], [Vector{Letter}(undef, 0) for _ in 1:rows]
 
 for y in 1:height
     for x in 1:width
-        push!(nesw[(y + x) - 1], wordsearch[x, y])
-        push!(nwse[(y - x) + height], wordsearch[x, y])
+        letter = Letter(wordsearch[x, y], x, y)
+        push!(nwse[(y - x) + height], letter)
+        push!(nesw[(y + x) - 1], letter)
     end
 end
 
+maslist = []
+
 for diagonal in nesw
-    global part1
-    part1 += search(diagonal)
+    global part1, maslist
+    maslist = vcat(maslist, searchmas(diagonal))
+    part1 += search([letter.score for letter in diagonal])
 end
 
 for diagonal in nwse
-    global part1
-    part1 += search(diagonal)
+    global part1, part2
+    for letter in searchmas(diagonal)
+        if letter in maslist # look for intersections
+            part2 += 1
+        end
+    end
+    part1 += search([letter.score for letter in diagonal])
 end
 
-println(part1) # not done yet
+println("part1: $(part1)")
+println("part2: $(part2)")
---
+++ day/6/main.php
@@ -0,0 +1,4 @@
+<?php
+    $string = file_get_contents("./day/6/input.txt");
+    echo $string;
+?>
