e94d57461e29fda7cf1f67cd5d118c32eb0825d7
BarkerProoks <jonparkerbrooks@gmail.com>
2024-12-10 19:06:55 +0000

    starting on day 6

--- day/6/main.php
+++ day/6/main.php
@@ -1,4 +1,22 @@
 <?php
-    $string = file_get_contents("./day/6/input.txt");
-    echo $string;
+    class Vec2 { public $x, $y; }
+    
+    $text = file_get_contents("./day/6/input.txt");
+    $lines = explode(PHP_EOL, $text);
+    $gaurd = new Vec2();
+
+    $height = count($lines);
+    $width = strlen($lines[0]);
+
+    foreach ($lines as $y => $line) {
+        foreach (str_split($line) as $x => $char) {
+            if ( $char === '^' ) {
+                $gaurd->x = $x;
+                $gaurd->y = $y;
+            }
+        }
+    }
+
+    echo $gaurd->x . ", " . $gaurd->y . PHP_EOL;
+    
 ?>
