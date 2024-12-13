dd003a184834a7356559b1a3f40fd711e0236310
BarkerProoks <jonparkerbrooks@gmail.com>
2024-12-13 12:29:00 -0600

    day 8

--- day/8/main.lisp
+++ day/8/main.lisp
@@ -1,1 +1,7 @@
-(format t "hello world~%")
+(let ((in (open "./day/8/input.txt")))
+    (loop for line = (read-line in nil)
+        while line do (format t "~a~%" line))
+    (close in))
+
+(dotimes (i 8)
+    (format t "~a~%" i))
