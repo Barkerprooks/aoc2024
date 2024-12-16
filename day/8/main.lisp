3d36930e1a60954614e74ad3cc7b5facc6394433
BarkerProoks <jonparkerbrooks@gmail.com>
2024-12-16 22:43:46 +0000

    day 8 progress

--- day/8/main.lisp
+++ day/8/main.lisp
@@ -1,7 +1,33 @@
-(let ((in (open "./day/8/input.txt")))
-    (loop for line = (read-line in nil)
-        while line do (format t "~a~%" line))
-    (close in))
+(require "asdf")
 
-(dotimes (i 8)
-    (format t "~a~%" i))
+(defun load-matrix (filename)
+    (let ((matrix `()))
+        (loop for line in (uiop:read-file-lines filename) and y from 0
+            do
+            (push `() matrix)
+            (loop for letter across line and x from 0
+                do (if (letter) (push letter matrix))))
+        matrix))
+
+; load the file as a hash map of symbols and where they appear on the map
+; we can just iterate through each finding their rise / run and placing
+; anodes at the relevent points along the line
+(defun load-antenae (matrix w h)
+    (let ((antenae (make-hash-table)))
+        (loop :for y :from 0 :to (- w 1)
+            do (loop :for x :from 0 :to (- h 1)
+                do (setf (gethash sym antenae) (cons (cons x y) (gethash sym antenae)))))
+        antenae))
+
+; slope formula given two tuples (x y) (x y)
+(defun slope (v0 v1)
+    (cons (- (car v0) (car v1)) (- (cdr v0) (cdr v1))))
+
+; (maphash (lambda (sym pairs)
+;     (loop for i from 0 to (- (length pairs) 1)
+;         do (loop for j from 0 to (- (length pairs) 1) 
+;             do (if (and (/= i j) (string/= sym ".")) 
+;                 (format t "~a: ~a~%" sym (slope (nth i pairs) (nth j pairs)))))))
+;     (load-antenae (load-matrix "./day/8/input.txt"))
+
+(print (load-matrix "./day/8/input.txt"))
