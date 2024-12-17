60596285bfe78293b26a0e5e0362882f1a8616a0
BarkerProoks <jonparkerbrooks@gmail.com>
2024-12-17 23:48:00 +0000

    started day 9

--- day/9/main.lisp
+++ day/9/main.lisp
@@ -1,1 +1,15 @@
-(format t "Hello world~%")
+(require "asdf")
+
+(defun load-map (filename)
+    (let ((id 0) (files nil))
+        (loop :for i :from 0 and n across (uiop:read-file-string filename)
+            do (if (evenp i)
+                (progn
+                    (dotimes (_ (digit-char-p n)) (push (write-to-string id) files))
+                    (setf id (+ id 1)))
+                (dotimes (_ (digit-char-p n)) (push "." files))))
+        files))
+
+(defparameter *xs* (load-map "./day/9/test.txt"))
+(dolist (x (reverse *xs*))
+   (format t "~a" x))
