4a4c0dbcc42a39e638b3ca876cf732e8f4032585
BarkerProoks <jonparkerbrooks@gmail.com>
2024-12-17 23:51:52 +0000

    clean up

--- day/9/main.lisp
+++ day/9/main.lisp
@@ -1,6 +1,7 @@
 (require "asdf")
 
-(defun load-map (filename)
+; unpacks the compressed string into its actual representation
+(defun load-file-map (filename)
     (let ((id 0) (files nil))
         (loop :for i :from 0 and n across (uiop:read-file-string filename)
             do (if (evenp i)
@@ -8,8 +9,13 @@
                     (dotimes (_ (digit-char-p n)) (push (write-to-string id) files))
                     (setf id (+ id 1)))
                 (dotimes (_ (digit-char-p n)) (push "." files))))
-        files))
+        (reverse files)))
 
-(defparameter *xs* (load-map "./day/9/test.txt"))
-(dolist (x (reverse *xs*))
+; TODO
+; returns a list of spaces with free spots
+(defun free-blocks (file-map) ())
+
+; show the list
+(dolist (x (load-file-map "./day/9/test.txt"))
    (format t "~a" x))
+(format t "~%")
