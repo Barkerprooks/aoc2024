4b10bf384821a79c01a7357c9eb51732caaedf9e
BarkerProoks <jonparkerbrooks@gmail.com>
2024-12-18 00:41:07 +0000

    so close

--- day/9/main.lisp
+++ day/9/main.lisp
@@ -3,19 +3,45 @@
 ; unpacks the compressed string into its actual representation
 (defun load-file-map (filename)
     (let ((id 0) (files nil))
-        (loop :for i :from 0 and n across (uiop:read-file-string filename)
+        (loop :for i :from 0 :and n :across (uiop:read-file-string filename)
             do (if (evenp i)
                 (progn
                     (dotimes (_ (digit-char-p n)) (push (write-to-string id) files))
                     (setf id (+ id 1)))
                 (dotimes (_ (digit-char-p n)) (push "." files))))
-        (reverse files)))
+        (make-array (length files) :initial-contents (reverse files))))
 
-; TODO
-; returns a list of spaces with free spots
-(defun free-blocks (file-map) ())
+; returns a list of indicies with free spots
+(defun free-blocks (file-map) 
+    (loop :for i :from 0 :and file :across file-map :if (string= file ".") collect i))
 
-; show the list
-(dolist (x (load-file-map "./day/9/test.txt"))
-   (format t "~a" x))
+; swaps blocks given two positions
+(defun swap-blocks (file-map a b)
+    (rotatef (aref file-map a) (aref file-map b)))
+
+; return the last non-null element from the array
+(defun last-non-null (file-map)
+    (loop :for i :from (- (length file-map) 1) :downto 0
+        :and block :across (reverse file-map)
+        :when (string/= block ".")
+            return i))
+
+(defparameter *file-map* (load-file-map "./day/9/test.txt"))
+
+(loop :for block :across *file-map* 
+    do (format t "~a" block))
+(format t "~%")
+
+(format t "~a~%" (free-blocks *file-map*))
+
+(loop :for freeblock :in (free-blocks *file-map*)
+    do (progn
+        (swap-blocks *file-map* freeblock (last-non-null *file-map*)))
+        (progn 
+            (loop :for block :across *file-map* 
+                do (format t "~a" block))
+            (format t "~%")))
+
+(loop :for block :across *file-map* 
+    do (format t "~a" block))
 (format t "~%")
